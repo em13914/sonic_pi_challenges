bc1e29c2e9cfb04841c4ca3096e1bc985a863468
Eric <em13914@users.noreply.github.com>
2024-02-08 13:14:25 -0800

    Create challengeA.rb
    live_loops run concurrently and can be used to created a layered sound in Sonic Pi!
    
    each live_loop runs in its own thread!

---
+++ challengeA.rb
@@ -0,0 +1,38 @@
+#CHALLENGE A: Stranger Things Layers
+use_bpm 160
+use_synth :saw
+
+#Layer 1
+live_loop :heartbeat do
+  sample :drum_heavy_kick, amp: 0.75
+  sleep 0.5
+  sample :drum_heavy_kick, amp: 0.75
+  sleep 3.5
+end
+
+#Layer 2
+live_loop :spooky do
+  sample :vinyl_backspin, beat_stretch: 8, amp: 2, rate: 1
+  sleep 8
+end
+
+#Layer 3
+live_loop :main_theme do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
