044088236d9f4e354cfe08c51333704486871d7f
Eric <em13914@users.noreply.github.com>
2024-02-08 18:37:22 -0800

    Update challengeA.rb
    comments updated to include idea of multiple threads

--- challengeA.rb
+++ challengeA.rb
@@ -2,7 +2,7 @@
 use_bpm 160
 use_synth :saw
 
-#Layer 1
+#Layer 1 immediately starts in its own thread
 live_loop :heartbeat do
   sample :drum_heavy_kick, amp: 0.75
   sleep 0.5
@@ -10,13 +10,13 @@ live_loop :heartbeat do
   sleep 3.5
 end
 
-#Layer 2
+#Layer 2 immediately starts in its own thread
 live_loop :spooky do
   sample :vinyl_backspin, beat_stretch: 8, amp: 2, rate: 1
   sleep 8
 end
 
-#Layer 3
+#Layer 3 immediately starts in its own thread
 live_loop :main_theme do
   play :c2
   sleep 0.5
