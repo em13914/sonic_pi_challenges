d519ac6f3f97eed10169781c14d7235807c90ef6
Eric <em13914@users.noreply.github.com>
2024-03-06 11:46:13 -0800

    Create challengeH.rb
    Can you optimize this code using a parameterized function?

---
+++ challengeH.rb
@@ -0,0 +1,148 @@
+=begin
+
+Functions are great for when the pattern of notes (sleeps) and the actual notes are exactly the same.
+
+Parameterised functions are great for when the pattern of notes is the same but the actual notes are different.
+
+YOUR TASK: Use a parameterised function to optimize the code below.
+
+=end
+
+use_bpm 120
+use_synth :chiplead
+
+
+# MEASURE 1 ---------------------------
+play :a4
+play :a5
+sleep 0.5
+
+play :a4
+play :a5
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :a4
+play :a5
+sleep 0.5
+
+play :d5
+play :d6
+sleep 0.5
+
+play :a4
+play :a5
+sleep 0.5
+
+play :e5
+play :e6
+sleep 0.5
+
+play :d5
+play :d6
+sleep 0.5
+
+
+# MEASURE 2 ---------------------------
+play :c5
+play :c6
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :e5
+play :e6
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :g5
+play :g6
+sleep 0.5
+
+play :c4
+play :c5
+sleep 0.5
+
+play :e5
+play :e6
+sleep 0.5
+
+play :c4
+play :c5
+sleep 0.5
+
+
+# MEASURE 3 ---------------------------
+play :g4
+play :g5
+sleep 0.5
+
+play :g4
+play :g5
+sleep 0.5
+
+play :b4
+play :b5
+sleep 0.5
+
+play :g4
+play :g5
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :g4
+play :g5
+sleep 0.5
+
+play :d5
+play :d6
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+
+# MEASURE 4 ---------------------------
+play :f4
+play :f5
+sleep 0.5
+
+play :f4
+play :f5
+sleep 0.5
+
+play :a4
+play :a5
+sleep 0.5
+
+play :f4
+play :f5
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :f4
+play :f5
+sleep 0.5
+
+play :c5
+play :c6
+sleep 0.5
+
+play :b4
+play :b5
+sleep 0.5
