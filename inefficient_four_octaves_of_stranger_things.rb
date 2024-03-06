f83b0dd587b9d26c5a945639cb4d61134912f669
Eric <em13914@users.noreply.github.com>
2024-03-06 11:40:31 -0800

    Create inefficient_four_octaves_of_stranger_things.rb

---
+++ inefficient_four_octaves_of_stranger_things.rb
@@ -0,0 +1,76 @@
+# Stranger Things Main Theme
+use_bpm 160
+use_synth :saw
+
+live_loop :main_theme do
+  with_fx :distortion do
+    #second octave
+    play :c2
+    sleep 0.5
+    play :e2
+    sleep 0.5
+    play :g2
+    sleep 0.5
+    play :b2
+    sleep 0.5
+    play :c3
+    sleep 0.5
+    play :b2
+    sleep 0.5
+    play :g2
+    sleep 0.5
+    play :e2
+    sleep 0.5
+    #third octave
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    #fourth octave
+    play :c4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    #fifth octave
+    play :c5
+    sleep 0.5
+    play :e5
+    sleep 0.5
+    play :g5
+    sleep 0.5
+    play :b5
+    sleep 0.5
+    play :c6
+    sleep 0.5
+    play :b5
+    sleep 0.5
+    play :g5
+    sleep 0.5
+    play :e5
+    sleep 0.5
+  end
+end
